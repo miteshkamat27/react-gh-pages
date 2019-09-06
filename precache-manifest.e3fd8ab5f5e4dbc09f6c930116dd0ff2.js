be146005b3e292282784e887ca216dab7f7e80f6
Mitesh Kamat <mitesh.kamat@ril.com>
2019-09-06 18:53:17 +0530

    Updates

---
+++ precache-manifest.e3fd8ab5f5e4dbc09f6c930116dd0ff2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "218e57ffc2244415e89f66cbc0a8d4ba",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "7250ce12cc525bd75efb",
+    "url": "/react-gh-pages/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a4210f686fb3fb8682d9",
+    "url": "/react-gh-pages/static/js/2.fcfad392.chunk.js"
+  },
+  {
+    "revision": "7250ce12cc525bd75efb",
+    "url": "/react-gh-pages/static/js/main.46578d4a.chunk.js"
+  },
+  {
+    "revision": "cd60c3dada059fda633e",
+    "url": "/react-gh-pages/static/js/runtime~main.f2d17e16.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
